33621e4a9c3c01bc223fd33a0e9687d62dd4e9f6
ShadowsKiller <lil_rom@orange.fr>
2013-10-20 22:03:59 +0200

    2ème commit --pour la forme

--- Uno_Bierge_Gagnaire/src/testPackage/TestClass.java
+++ Uno_Bierge_Gagnaire/src/testPackage/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//This is the first [hopefully working] testsqdfqdfs
+		//This is the first [hopefully working] test
 	}
 
 }
