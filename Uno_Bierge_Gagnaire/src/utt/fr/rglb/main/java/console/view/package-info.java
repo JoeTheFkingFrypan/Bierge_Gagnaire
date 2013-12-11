741054d6e939318ac2077ab591d436077a624f2b
JoeTheFkingFrypan <lil_rom@orange.fr>
2013-12-11 23:28:46 +0100

    Shit just got real, again

---
+++ Uno_Bierge_Gagnaire/src/utt/fr/rglb/main/java/console/view/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package associé à la vue liée à la console
+ */
+package utt.fr.rglb.main.java.console.view;
---
+++ Uno_Bierge_Gagnaire/src/utt/fr/rglb/tests/java/cards/controller/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package associé aux tests basés sur le controlleur permettant de gérer l'ensemble des cartes composant le jeu
+ */
+package utt.fr.rglb.tests.java.cards.controller;
